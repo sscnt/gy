b36b8158bdf5b554e12da517fc71becb27ef57a6
sscnt <pixeltc@gmail.com>
2013-11-10 01:51:38 +0900

    Lab finished

--- Gravy_1.0/GPUImageSelectiveColorFilter.m
+++ Gravy_1.0/GPUImageSelectiveColorFilter.m
@@ -61,7 +61,74 @@ NSString *const kGPUImageSelectiveColorFilterFragmentShaderString = SHADER_STRIN
  uniform highp float blacksYellow;
  uniform highp float blacksBlack;
  
+ vec3 rgb2xyz(highp vec3 color){
+     mat3 adobe;
+     adobe[0] = vec3(0.576669, 0.297345, 0.027031);
+     adobe[1] = vec3(0.185558, 0.627364, 0.070689);
+     adobe[2] = vec3(0.188229, 0.075291, 0.991338);
+     mat3 srgb;
+     srgb[0] = vec3(0.412391, 0.212639, 0.019331);
+     srgb[1] = vec3(0.357584, 0.715169, 0.119195);
+     srgb[2] = vec3(0.180481, 0.072192, 0.950532);
+     return srgb * color;
+ }
  
+ vec3 xyz2rgb(highp vec3 color){
+     mat3 adobe;
+     adobe[0] = vec3(2.041588, -0.969244, 0.013444);
+     adobe[1] = vec3(-0.565007, 1.875968, -0.118362);
+     adobe[2] = vec3(-0.344731, 0.041555, 1.015175);
+     mat3 srgb;
+     srgb[0] = vec3(3.240970 , -0.969244  , 0.055630 );
+     srgb[1] = vec3(-1.537383 , 1.875968  , -0.203977  );
+     srgb[2] = vec3(-0.498611, 0.041555, 1.056972);
+     return srgb * color;
+ }
+ 
+ float xyz2labFt(highp float t){
+     if(t > 0.00885645167){
+         return 116.0 * pow(t, 0.33333333333) - 16.0;
+     } else{
+         return 903.296296296 * t;
+     }
+ }
+ 
+ vec3 xyz2lab(highp vec3 color){
+     highp float l = xyz2labFt(color.y);
+     highp float a = 4.31034482759 * (xyz2labFt(color.x / 0.9642) - l);
+     highp float b = 1.72413793103 * (l - xyz2labFt(color.z / 0.8249));
+     return vec3(l, a, b);
+ }
+ 
+ vec3 lab2xyz(highp vec3 color){
+     highp float fy = (color.x + 16.0) / 116.0;
+     highp float fx = fy + (color.y / 500.0);
+     highp float fz = fy - (color.z / 200.0);
+     highp float x;
+     highp float y;
+     highp float z;
+     if(fy > 0.20689655172)
+         y = fy * fy * fy;
+     else
+         y = 0.00110705645 * (116.0 * fy - 16.0);
+     if(fx > 0.20689655172)
+         x = fx * fx * fx * 0.9642;
+     else
+         x = 0.00110705645 * (116.0 * fx - 16.0) * 0.9642;
+     if(fz > 0.20689655172)
+         z = fz * fz * fz * 0.8249 ;
+     else
+         z = 0.00110705645 * (116.0 * fz - 16.0) * 0.8249;
+     return vec3(x, y, z);
+ }
+ 
+ vec3 rgb2lab(highp vec3 color){
+     return xyz2lab(rgb2xyz(color));
+ }
+ 
+ vec3 lab2rgb(highp vec3 color){
+     return xyz2rgb(lab2xyz(color));
+ }
  
  void main()
  {
@@ -70,9 +137,9 @@ NSString *const kGPUImageSelectiveColorFilterFragmentShaderString = SHADER_STRIN
      highp float r = pixel.r;
      highp float g = pixel.g;
      highp float b = pixel.b;
-     highp float ra;
-     highp float ga;
-     highp float ba;
+     highp float ra = r;
+     highp float ga = g;
+     highp float ba = b;
      
      // Convert to CMYK
      highp float c = 1.0 - r;
@@ -89,66 +156,36 @@ NSString *const kGPUImageSelectiveColorFilterFragmentShaderString = SHADER_STRIN
          y = (y - k) / (1.0 - k);
      }
      
-     // Convert to XYZ
-     highp float x_;
-     highp float y_;
-     highp float z_;
-     if(r > 0.04045)
-         ra = pow((r + 0.055) / 1.055, 2.4);
-     else
-         ra = r / 12.92;
-     if(g > 0.04045)
-         ga = pow((g + 0.055) / 1.055, 2.4);
-     else
-         ga = g / 12.92;
-     if(b > 0.04045)
-         ba = pow((b + 0.055) / 1.055, 2.4);
-     else
-         ba = b / 12.92;
-     ra *= 100.0;
-     ga *= 100.0;
-     ba *= 100.0;
-     
-     x_ = ra * 0.4124 + ga * 0.3576 + ba * 0.1805;
-     y_ = ra * 0.2126 + ga * 0.7152 + ba * 0.0722;
-     z_ = ra * 0.0193 + ga * 0.1192 + ba * 0.9505;
-     
-     // Convert to CIE-L*ab
-     highp float l_;
-     highp float a_;
-     highp float b_;
-     x_ /= 95.047;
-     y_ /= 100.000;
-     z_ /= 108.883;
-     if(x_ > 0.008856)
-         x_ = pow(x_, 0.33333333333);
-     else
-         x_ = (7.78703703704 * x_) + 0.13793103448;
-     if(y_ > 0.008856)
-         y_ = pow(y_, 0.33333333333);
-     else
-         y_ = (7.78703703704 * y_) + 0.13793103448;
-     if(z_ > 0.008856)
-         z_ = pow(z_, 0.33333333333);
-     else
-         z_ = (7.78703703704 * z_) + 0.13793103448;
-     l_ = (116.0 * y_) - 16.0;
-     a_ = 500.0 * (x_ - y_);
-     b_ = 200.0 * (y_ - z_);
-     
-     
-     highp float redsWeight = r * (1.0 - g) * (1.0 - b);
-     redsWeight = max(0.0, (r - abs(g - b))) * max(0.0, (r - abs(g - b))) * (1.0 - abs(g - b));
-     redsWeight = max(0.0, (r - max(g, b)));
-     //redsWeight = max(0.0, (r - g)) * max(0.0, (r - b)) * r * (1.0 - abs(g - b));
-     //redsWeight = max(0.0, (r - g)) * max(0.0, (r - b));
 
+     // Convert to CIE-L*ab
+     highp vec3 lab = rgb2lab(vec3(r, g, b));
+     highp float ll = lab.x;
+     highp float al = lab.y;
+     highp float bl = lab.z;
+     
+     highp vec3 redLab = rgb2lab(vec3(1.0, 0.0, 0.0));
+     highp float redsWeight = sqrt((ll - redLab.x) * (ll - redLab.x) + (al - redLab.y) * (al - redLab.y) + (bl - redLab.z) * (bl - redLab.z)) / 176.0;
+     redsWeight = 1.0 - min(redsWeight, 1.0);
+     
+     
+
+     
+     ra -= (1.0 - r) * redsCyan * redsWeight;
+     ga -= (1.0 - g) * redsMagenta * redsWeight;
+     ba -= (1.0 - b) * redsYellow * redsWeight;
      
 
      
      /*
      
  
+     highp float redsWeight = r * (1.0 - g) * (1.0 - b);
+     redsWeight = max(0.0, (r - abs(g - b))) * max(0.0, (r - abs(g - b))) * (1.0 - abs(g - b));
+     redsWeight = max(0.0, (r - max(g, b)));
+     //redsWeight = max(0.0, (r - g)) * max(0.0, (r - b)) * r * (1.0 - abs(g - b));
+     //redsWeight = max(0.0, (r - g)) * max(0.0, (r - b));
+
+
      
      c += (1.0 - c) * redsCyan * redsWeight;
      m += (1.0 - m) * redsMagenta * redsWeight;
@@ -161,58 +198,15 @@ NSString *const kGPUImageSelectiveColorFilterFragmentShaderString = SHADER_STRIN
      ra = (1.0 - c);
      ga = (1.0 - m);
      ba = (1.0 - y);
-     
-     
-     ra -= (1.0 - r) * redsCyan * redsWeight;
-     ga -= (1.0 - g) * redsMagenta * redsWeight;
-     ba -= (1.0 - b) * redsYellow * redsWeight;
-     
-     ra -= (1.0 - r) * redsBlack * redsWeight;
+          ra -= (1.0 - r) * redsBlack * redsWeight;
      ga -= (1.0 - g) * redsBlack * redsWeight;
      ba -= (1.0 - b) * redsBlack * redsWeight;
 
+     ra = distance;
+     ga = distance;
+     ba =
+     distance;
     */
-     
-     // Convert to XYZ
-     y_ = (l_ + 16.0) / 116.0;
-     x_ = a_ / 500.0 + y;
-     z_ = y_ - b_ / 200.0;
-     if(y_ > 0.20689655172)
-         y_ = y_*y_*y_;
-     else
-         y_ = (y_ - 0.13793103448) / 7.78703703704;
-     if(x_ > 0.20689655172)
-         x_ = x_*x_*x_;
-     else
-         x_ = (x_ - 0.13793103448) / 7.78703703704;
-     if(z_ > 0.20689655172)
-         z_ = z_*z_*z_;
-     else
-         z_ = (z_ - 0.13793103448) / 7.78703703704;
-     x_ *= 95.047;
-     y_ *= 100.000;
-     z_ *= 108.883;
-     
-     // Convert to RGB
-     x_ /= 100.0;
-     y_ /= 100.0;
-     z_ /= 100.0;
-     
-     ra = x_ * 3.2406 + y_ * -1.5372 + z_ * -0.4986;
-     ga = x_ * -0.9689 + y_ * 1.8758 + z_ * 0.0415;
-     ba = x_ * 0.0557 + y_ * -0.2040 + z_ * 1.0570;
-     if(ra > 0.0031308)
-         ra = 1.055 * pow(ra, 0.41666666666) - 0.055;
-     else
-         ra *= 12.92;
-     if(ga > 0.0031308)
-         ga = 1.055 * pow(ga, 0.41666666666) - 0.055;
-     else
-         ga *= 12.92;
-     if(ba > 0.0031308)
-         ba = 1.055 * pow(ba, 0.41666666666) - 0.055;
-     else
-         ba *= 12.92;
      
      
      // Check
